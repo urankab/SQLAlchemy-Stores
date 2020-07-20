7432806bf150b3c8d221226c2a3c84932bedba12
Uranka <39204253+urankab@users.noreply.github.com>
2020-07-20 16:56:56 -0600

    Created run.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
-from db import db
+# from db import db
 
 from security import authenticate, identity
 from resources.user import UserRegister
@@ -27,5 +27,6 @@ api.add_resource(StoreList, '/stores')
 api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
+    from db import db
     db.init_app(app)
     app.run(port=5000, debug=True)
